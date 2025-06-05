916df52569ddcdb20fb4e9187004ea2f86cdb6a3
Anton Tetov <anton@tetov.se>
2025-06-05 13:39:22 +0200

    nit: settings updated from printing and setup changes outside this repo

--- src/clay_3dp_print/__main__.py
+++ src/clay_3dp_print/__main__.py
@@ -4,31 +4,33 @@ from typing import Generator
 import compas.geometry
 import compas_rrc as rrc
 from compas import json_load
+from compas.geometry import Frame
 from compas_fab.backends import RosClient
 from compas_fab.backends.ros.messages import ROSmsg
 
-from clay_3dp_print import PrintLayer
+from clay_3dp_print import PrintFrame, PrintLayer
 
 # IP = "192.168.8.30" # cook
 IP = "localhost"
 
 WOBJ = "wobj_pallet_markers"
 TOOL = "t_erratic_t25"
-
-SPEED = 75
+SPEED = 150
 SPEED_PRINT = 50
 
 EXTRUSION_FACTOR_AO = "ao_printPtSpd"
 
-# negative means move towards the sky
-Z_ADJUSTMENT = 0
+# negative Z means move towards the sky
+XYZ_ADJUSTMENT = (0, 0, 6)
+
+Z_HOP = -50
 
 
 def get_set_extruder(speed_factor: float) -> ROSmsg:
     return rrc.SetAnalog(EXTRUSION_FACTOR_AO, speed_factor)
 
 
-def get_start_extrude(speed_factor: float = 1.0) -> ROSmsg:
+def get_start_extrude(speed_factor: float = 1.0):
     return get_set_extruder(speed_factor)
 
 
@@ -37,38 +39,79 @@ def get_stop_extrude() -> ROSmsg:
 
 
 def construct_cmds(layers: list[PrintLayer]) -> Generator[ROSmsg]:
-    first_print_frame = layers[0].pop(0)
+    f = layers[0][0]
+    z_hop_first_layer = PrintFrame(Frame(f.point, xaxis=f.xaxis, yaxis=f.yaxis), 0)
+    # z_hop_first_layer = PrintFrame(f.copy(), 0)
 
-    first_print_frame.translate_frame_in_local_Z(Z_ADJUSTMENT)
+    z_hop_first_layer.translate_frame_in_local_Z(Z_HOP)
+
+    print(f"First before print Z: {z_hop_first_layer.zaxis}")
 
     yield rrc.MoveToFrame(
-        first_print_frame, SPEED, rrc.Zone.FINE, motion_type=rrc.Motion.JOINT
+        z_hop_first_layer, SPEED, rrc.Zone.FINE, motion_type=rrc.Motion.JOINT
     )
 
+    last_was_travel = False
     for i, layer in enumerate(layers):
         yield rrc.PrintText(f"Layer {i}")
 
-        x, y, z = layer[0].point
+        first_frame = layer[0]
+        x, y, z = first_frame.point
         first_pt_str = f"{x:.2f},{y:.2f},{z:.2f}"
 
         yield rrc.PrintText(f"First frame: {first_pt_str}")
 
+        z_hop_frame = PrintFrame(Frame(f.point, xaxis=f.xaxis, yaxis=f.yaxis), 0)
+
+        z_hop_frame.translate_frame_in_local_Z(Z_HOP)
+        yield rrc.MoveToFrame(
+            z_hop_frame,
+            SPEED,
+            rrc.Zone.Z10,
+            motion_type=rrc.Motion.JOINT,
+            feedback_level=rrc.FeedbackLevel.DONE,
+        )
+        yield rrc.MoveToFrame(
+            first_frame,
+            SPEED,
+            rrc.Zone.FINE,
+            motion_type=rrc.Motion.LINEAR,
+            feedback_level=rrc.FeedbackLevel.DONE,
+        )
+
         for print_frame in layer:
             yield get_set_extruder(print_frame.extrusion_factor)
 
-            print_frame.translate_frame_in_local_Z(Z_ADJUSTMENT)
+            print_frame.translate_frame_in_local_Z(XYZ_ADJUSTMENT[2])
 
-            if print_frame.is_travel():
+            if print_frame.is_travel() and last_was_travel:
                 # TODO: Add retraction
                 pass
+
+            last_was_travel = print_frame.is_travel()
 
             yield rrc.MoveToFrame(
                 print_frame,
                 SPEED if print_frame.is_travel() else SPEED_PRINT,
-                rrc.Zone.Z1,
+                rrc.Zone.Z2,
                 motion_type=rrc.Motion.LINEAR,
                 feedback_level=rrc.FeedbackLevel.DONE,
             )
+
+        yield get_set_extruder(0)
+
+        z_hop_frame = PrintFrame(
+            Frame(print_frame.point, print_frame.xaxis, print_frame.yaxis), 0
+        )
+        z_hop_frame.translate_frame_in_local_Z(Z_HOP)
+
+        yield rrc.MoveToFrame(
+            z_hop_frame,
+            SPEED,
+            rrc.Zone.Z10,
+            motion_type=rrc.Motion.JOINT,
+            feedback_level=rrc.FeedbackLevel.DONE,
+        )
 
 
 def process_with_batches(
@@ -120,7 +163,7 @@ def process_with_batches(
 
 def robot_program(layers: list[PrintLayer]):
     # Define robot joints
-    robot_joints_start_position = robot_joints_end_position = [100, 20, 25, 5, -40, -10]
+    robot_joints_start_position = robot_joints_end_position = [-71, 8, 34, 99, -94, -58]
 
     # Define external axis
     external_axis_dummy = []
